5c2d4866902f82405b029963f708bb94225600bf
swx316 <623281247@qq.com>
2014-10-20 17:04:02 +0800

    game2048

---
+++ src/com/example/view/GameView.java
@@ -0,0 +1,12 @@
+package com.example.view;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView extends View {
+
+	public GameView(Context context) {
+		super(context);
+	}
+
+}
