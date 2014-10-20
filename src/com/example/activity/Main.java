808888726cbe92a58d5df85999f65a5ffb55c3cb
swx316 <623281247@qq.com>
2014-10-20 17:41:20 +0800

    u

--- src/com/example/activity/Main.java
+++ src/com/example/activity/Main.java
@@ -1,5 +1,7 @@
 package com.example.activity;
 
+import com.example.view.GameView;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,9 +9,13 @@ import android.view.MenuItem;
 
 public class Main extends Activity {
 
+	private GameView gameview;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		gameview=(GameView) findViewById(R.id.gameview);
+		
 	}
 }
